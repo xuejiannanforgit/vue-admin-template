038e1ceb045079728e958683ae8094870fe8c962
xuejiannan <330288681@qq.com>
2023-06-28 23:27:52 +0800

    second commit

--- vue-admin-template-4.2.1/src/store/index.js
+++ vue-admin-template-4.2.1/src/store/index.js
@@ -4,16 +4,30 @@ import getters from './getters'
 import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
+import createPersistedState from 'vuex-persistedstate'
+
 
 Vue.use(Vuex)
 
+// 创建PERSIST_PATHS变量 存储要持久化的模块
+const PERSIST_PATHS = ['user']
 const store = new Vuex.Store({
+  state: {},
   modules: {
     app,
     settings,
-    user
+    user,
+    permission,
+    tagsView
   },
-  getters
+  getters,
+  // 新增规则保存vuex的值
+  plugins: [createPersistedState({
+    storage: window.sessionStorage,
+    // 在此使用
+    paths: PERSIST_PATHS
+  })]
 })
+
 
 export default store
